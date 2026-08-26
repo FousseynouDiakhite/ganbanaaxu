1ad26f3789506f5b0ee5280bc75a016ea49c9cfd
Fousseynou <fousseynoudiakhite565@gmail.com>
2026-08-26 15:08:35 +0000

    Mise à jour du versionCode à 53 pour Google Play

--- app.config.js
+++ app.config.js
@@ -100,7 +100,7 @@ export default {
   expo: {
     name: "Ganbanaaxu",
     slug: "Ganbanaaxu",
-    version: "1.0.3",
+    version: "1.0.4",
     orientation: "portrait",
     icon: "./assets/images/logoLiberty.png",
     scheme: "ganbanaaxu",
@@ -112,7 +112,7 @@ export default {
     android: {
       package: "com.godapps.Ganbanaaxu",
       googleServicesFile: "./google-services.json",
-      versionCode: 53, // N'oubliez pas de mettre 49 si Google Play refuse la version 48
+      versionCode: 54, // N'oubliez pas de mettre 49 si Google Play refuse la version 48
       /*targetSdkVersion: 36,
       compileSdkVersion: 36,*/
       usesCleartextTraffic: true,
