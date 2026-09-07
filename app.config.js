efe7c1e3831ae38949d04c2d08bb8ac775302620
Fousseynou <fousseynoudiakhite565@gmail.com>
2026-09-07 20:17:46 +0000

    build de release

--- app.config.js
+++ app.config.js
@@ -198,7 +198,7 @@ export default {
   expo: {
     name: "Ganbanaaxu",
     slug: "Ganbanaaxu",
-    version: "1.0.10",
+    version: "1.0.11",
     orientation: "portrait",
     icon: "./assets/images/logoLiberty.png",
     scheme: "ganbanaaxu",
@@ -210,7 +210,7 @@ export default {
     android: {
       package: "com.godapps.Ganbanaaxu",
       googleServicesFile: "./google-services.json",
-      versionCode: 59,
+      versionCode: 60,
       usesCleartextTraffic: true,
       adaptiveIcon: {
         foregroundImage: "./assets/images/logoLiberty.png",
