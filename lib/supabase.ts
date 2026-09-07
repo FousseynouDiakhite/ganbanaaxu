cadc32c791f3e10af8a8991bc360e821d8010e76
Fousseynou <fousseynoudiakhite565@gmail.com>
2026-09-07 10:45:53 +0000

    chore: declencher un nouveau build de release

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,4 +1,4 @@
-
+/*
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -14,3 +14,32 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   },
 });
 
+*/
+
+
+
+import 'react-native-url-polyfill/auto';
+import { AppState } from 'react-native';
+import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://tuciyiawyawrhifpjmmn.supabase.co';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_HnPHoEeulclLH-AIHOhS-w_y9j6oZo1';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
+
+// Gère le cycle de vie de l'application pour éviter les bugs de token après une période hors ligne
+AppState.addEventListener('change', (state) => {
+  if (state === 'active') {
+    supabase.auth.startAutoRefresh();
+  } else {
+    supabase.auth.stopAutoRefresh();
+  }
+});
