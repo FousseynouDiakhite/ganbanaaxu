2162ce2e97eaf5a1bb650206c5b00b17543e7fb6
Fousseynou <fousseynoudiakhite565@gmail.com>
2026-09-16 17:42:13 +0000

    un nouveau build de release

--- app.config.js
+++ app.config.js
@@ -251,8 +251,8 @@ export default {
       [
         "react-native-google-mobile-ads",
         {
-          "androidAppId": "ca-app-pub-3940256099942544~3347511713",
-          "iosAppId": "ca-app-pub-3940256099942544~1458002511"
+          "androidAppId": "ca-app-pub-2071663229767228/8501336070",
+          /*"iosAppId": "ca-app-pub-2071663229767228/8501336070"*/
         }
       ],
       [
