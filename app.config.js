e48a5765c6792d797e8abaaf76cf04e623250073
Fousseynou <fousseynoudiakhite565@gmail.com>
2026-09-17 21:59:54 +0000

    fix: rétrograde play-services-ads à 23.6.0 pour compatibilité Kotlin

--- app.config.js
+++ app.config.js
@@ -457,7 +457,7 @@ export default {
         "expo-build-properties",
         {
           android: {
-            kotlinVersion: "2.3.0",
+            kotlinVersion: "2.1.0",
         }
       ],
       [
