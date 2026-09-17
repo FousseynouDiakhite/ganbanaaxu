89a626cc635c14be4275d08ebfd2e51934de5f44
Fousseynou <fousseynoudiakhite565@gmail.com>
2026-09-17 22:06:59 +0000

    fix: correction syntaxique complète du app.config.js

--- app.config.js
+++ app.config.js
@@ -416,6 +416,11 @@ export default {
 
 
 
+
+
+
+
+
 export default {
   expo: {
     name: "Ganbanaaxu",
@@ -457,7 +462,8 @@ export default {
         "expo-build-properties",
         {
           android: {
-            kotlinVersion: "2.1.0",
+            kotlinVersion: "2.1.0"
+          }
         }
       ],
       [
@@ -481,7 +487,6 @@ export default {
           androidAppId: "ca-app-pub-3940256099942544~3347511713",
           iosAppId: "ca-app-pub-3940256099942544~1458002511",
           playServicesAdsVersion: "23.6.0"
-      }
         }
       ],
       [
@@ -517,6 +522,8 @@ export default {
     owner: "godapps"
   }
 };
+
+
 
 
 
