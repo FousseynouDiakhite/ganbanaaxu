20d69c6c70c9bd2003b843af9b1733fc37a456f0
Fousseynou <fousseynoudiakhite565@gmail.com>
2026-09-17 18:45:53 +0000

    un nouveau build de release

--- app.config.js
+++ app.config.js
@@ -457,7 +457,7 @@ export default {
         "expo-build-properties",
         {
           android: {
-            kotlinVersion: "2.1.20"
+            kotlinVersion: "2.3.0"
           }
         }
       ],
